4d32278d8cec1293cda10f041f7e09fc5b0d674c
ZainDar12 <k250146@nu.edu.pk>
2026-02-20 19:39:34 +0500

    Add Car class with speed management

---
+++ Q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Car {
+private:
+    int speed;
+public:
+    void setSpeed(int s) {
+        speed = s;
+    }
+    void showSpeed() const {
+        cout << "Speed: " << speed << endl;
+    }
+};
+
+int main() {
+    Car c;
+    c.setSpeed(120);
+    c.showSpeed();
+    return 0;
+}
